219c9c0ae101206c9f6bcf92e62e9c7a7e8a5396
Ezra <ezraermy@gmail.com>
2023-05-18 00:09:04 +0300

    Add morse-message methods and readme file

--- morse_code.rb
+++ morse_code.rb
@@ -13,12 +13,23 @@ def decode_char(morse_code)
 end
 
 def decode_word(morse_word)
-  decoded_word = ""
-  morse_chars = morse_word.split(" ")
+  decoded_word = ''
+  morse_chars = morse_word.split
 
   morse_chars.each do |morse_char|
     decoded_word += decode_char(morse_char)
   end
 
-  return decoded_word
+  decoded_word
+end
+
+def decode_message(morse_message)
+  decoded_message = ''
+  morse_words = morse_message.split('   ')
+
+  morse_words.each do |morse_word|
+    decoded_message += "#{decode_word(morse_word)} "
+  end
+
+  decoded_message.strip
 end
